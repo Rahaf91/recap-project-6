fd394f84282292212e6af77f58e8995526f75a9c
Rahaf <rahafsekny@gmail.com>
2024-07-19 11:59:03 +0200

    update

--- db/models/Place.js
+++ db/models/Place.js
@@ -4,10 +4,10 @@ const { Schema } = mongoose;
 
 const placeSchema = new Schema({
   name: { type: String, required: true },
-  location: { type: String },
-  description: { type: String },
-  image: { type: String },
-  mapURL: { type: String },
+  location: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  mapURL: { type: String, required: true },
 });
 
 const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
